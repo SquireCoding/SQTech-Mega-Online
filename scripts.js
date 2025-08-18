bb7db68fbf12c7323b74ac40982cb648a9b5974c
SquireCoding <ben@holyjoemail.com>
2025-08-18 09:03:11 -0500

    Updated colors and whatnot

--- scripts.js
+++ scripts.js
@@ -1,5 +1,335 @@
-var cart=[]
+const itemsPage= `Stone-1/48=A natural grey rock used found very commonly underground, though silk touch is required to pick it up.^767676
+Granite-1/24=An rock used in some construction that is a unique red hue.^946452
+Polished Granite-0/0=A smooth variation of the original red rock.
+Diorite-1/24=
+Polished Diorite
+Andesite-1/24
+Polished Andesite
+Deepslate-1/32
+Cobbled Deepslate-1/12
+Polished Deepslate
+Calcite
+Tuff
+Dripstone Block-1/2
+Grass Block
+Dirt-1/48=An essential block for filling up holes and adding that natural look that's so... natural.^AC7C56
+Coarse Dirt
+Podzol
+Rooted Dirt
+Mud
+Muddy Mangrove Roots
+Clay
+Gravel-1/10
+Sand-1/10
+Red Sand
+Soul Sand-1/8
+Soul Soil
+Mycelium
+Crimson Nylium
+Warped Nylium
+Netherrack-1/32
+Basalt
+Polished Basalt
+Smooth Basalt
+End Stone
+Purpur Block
+Purpur Pillar
+Purpur Stairs
+Purpur Slab
+Obsidian-1/4
+Crying Obsidian-1/2
+Water Bucket-3/2-b2
+Lava Bucket-3/2-b2
+Snow Block-1/12
+Ice-1/4
+Packed Ice-1/2
+Blue Ice-4/1
+Layer Snow-1/8
+Powder Snow Bucket-4/1-b2
+Sponge-10/1
+Wet Sponge
+Glass-1/2
+Glass Pane
+Stained Glass
+Stained Glass Pane
+Wool-1/3
+Carpet
+Terracotta-1/2
+Glazed Terracotta
+Concrete-1/4
+Concrete Powder-1/6
+Bricks-2/1
+Brick Slab
+Brick Stairs
+Brick Wall
+Stone Bricks
+Mossy Stone Bricks
+Cracked Stone Bricks
+Chiseled Stone Bricks
+Stone Brick Slab
+Stone Brick Stairs
+Stone Brick Wall
+Moss Block
+Moss Carpet
+Azalea
+Flowering Azalea
+Azalea Leaves
+Flowering Azalea Leaves
+Oak Log
+Stripped Oak Log
+Oak Wood
+Stripped Oak Wood
+Oak Planks
+Oak Slab
+Oak Stairs
+Oak Fence
+Oak Fence Gate
+Oak Door
+Oak Trapdoor
+Oak Pressure Plate
+Oak Button
+Oak Sign
+Oak Hanging Sign
+Spruce variants (all equivalents)
+Birch variants (all equivalents)
+Jungle variants (all equivalents)
+Acacia variants (all equivalents)
+Dark Oak variants (all equivalents)
+Mangrove variants (all equivalents)
+Cherry variants (all equivalents)
+Bamboo variants (all equivalents)
+Crimson variants (all equivalents)
+Warped variants (all equivalents)
+Leaves (Oak, Spruce, Birch, Jungle, Acacia, Dark Oak, Azalea, Flowering Azalea, Mangrove, Cherry)
+Saplings (all tree types)
+Flowers (Dandelion, Poppy, Blue Orchid, etc.)
+Tall Flowers (Rose Bush, Lilac, etc.)
+Mushrooms (Red, Brown)
+Nether Sprouts
+Crimson Fungus
+Warped Fungus
+Chorus Plant
+Chorus Flower
+Crops (Wheat, Carrots, Potatoes, Beetroot, Melon, Pumpkin)
+Seeds (Wheat, Melon, Pumpkin, Beetroot, Torchflower, Pitcher Pod)
+Nether Wart
+Cocoa Beans
+Sweet Berries
+Glow Berries
+Sugar Cane
+Cactus
+Bamboo
+Kelp
+Seagrass
+Sea Pickle
+Vines
+Glow Lichen
+Coral Blocks, Coral, Coral Fans (all 5 types)
+Amethyst Block
+Budding Amethyst
+Amethyst Shard
+Copper Ore
+Raw Copper
+Block of Raw Copper
+Copper Ingot
+Block of Copper
+Exposed Copper
+Weathered Copper
+Oxidized Copper
+Cut Copper (and all variants: exposed, weathered, oxidized, waxed)
+Iron Ore
+Raw Iron
+Block of Raw Iron
+Iron Ingot
+Block of Iron
+Gold Ore
+Raw Gold
+Block of Raw Gold
+Gold Ingot
+Block of Gold
+Nether Gold Ore
+Lapis Lazuli Ore
+Lapis Lazuli
+Block of Lapis Lazuli
+Redstone Ore
+Redstone Dust
+Block of Redstone
+Diamond Ore
+Diamond
+Block of Diamond
+Emerald Ore
+Emerald
+Block of Emerald
+Coal Ore
+Coal
+Charcoal
+Block of Coal
+Nether Quartz Ore
+Nether Quartz
+Block of Quartz
+Quartz Pillar
+Chiseled Quartz Block
+Smooth Quartz Block
+Ancient Debris
+Netherite Scrap
+Netherite Ingot
+Block of Netherite
+Armor (Leather, Chainmail, Iron, Gold, Diamond, Netherite; all pieces)
+Shields
+Elytra
+Tools (Wood, Stone, Iron, Gold, Diamond, Netherite; all types: sword, pickaxe, axe, shovel, hoe)
+Flint and Steel
+Shears
+Fishing Rod
+Carrot on a Stick
+Warped Fungus on a Stick
+Bow
+Crossbow
+Trident
+Arrows (normal, spectral, tipped)
+Totem of Undying
+Saddle
+Horse Armor (Leather, Iron, Gold, Diamond)
+Minecart
+Chest Minecart
+Furnace Minecart
+Hopper Minecart
+TNT Minecart
+Boat (all wood types)
+Chest Boat (all wood types)
+Rails (Rail, Powered Rail, Detector Rail, Activator Rail)
+Redstone Torch
+Redstone Repeater
+Redstone Comparator
+Lever
+Target Block
+Note Block
+Jukebox
+Hopper
+Dropper
+Dispenser
+Piston
+Sticky Piston
+Observer
+Daylight Detector
+Lightning Rod
+Tripwire Hook
+Bell
+Beacon
+Conduit
+Lodestone
+Respawn Anchor
+Anvil
+Chipped Anvil
+Damaged Anvil
+Grindstone
+Smithing Table
+Cartography Table
+Fletching Table
+Loom
+Stonecutter
+Enchanting Table
+Brewing Stand
+Cauldron
+Barrel
+Chest
+Ender Chest
+Shulker Box (all 16 colors)
+Furnace
+Blast Furnace
+Smoker
+Campfire
+Soul Campfire
+Lantern
+Soul Lantern
+Torch
+Soul Torch
+Scaffolding
+Ladder
+Bookshelf
+Chiseled Bookshelf
+Lectern
+Painting
+Item Frame
+Glow Item Frame
+Banner (all 16 colors)
+Banner Pattern Variants
+Bed (all 16 colors)
+Cake
+Cookie
+Bread
+Golden Apple
+Enchanted Golden Apple
+Honey Bottle
+Honeycomb
+Milk Bucket
+Suspicious Stew
+Stew/Soup Types
+Cooked and Raw Meats (Beef, Chicken, Porkchop, Mutton, Rabbit, Fish types)
+Rotten Flesh
+Spider Eye
+Fermented Spider Eye
+Phantom Membrane
+Blaze Rod
+Blaze Powder
+Ghast Tear
+Magma Cream
+Slimeball
+Ender Pearl
+Eye of Ender
+Shulker Shell
+Wither Skeleton Skull
+Dragon Head
+Nether Star
+Music Discs: 13, Cat, Blocks, Chirp, Far, Mall, Mellohi, Stal, Strad, Ward, 11, Wait, Pigstep, Otherside, 5, Relic, Creator, Creator (Music Box), Precipice
+Potions (all base types: Water, Awkward, Mundane, Thick)
+Potion Variants (Healing, Strength, Swiftness, Fire Resistance, Night Vision, Invisibility, Poison, Regeneration, Weakness, Harming, Slow Falling, Water Breathing, Turtle Master, Leaping, Slowness, Luck)
+Splash Potions (all variants)
+Lingering Potions (all variants)
+Tipped Arrows (all variants)
+Book
+Book and Quill
+`;
+const itemLines=itemsPage.split(/\r?\n/);
 
+var cart=[];
+
+function loadMainPage() {
+    for (let i=0; i<itemLines.length; i++) {
+        if (itemLines[i].includes("-")&&itemLines[i].includes("=")&&itemLines[i].includes("^")) {
+            document.getElementsByClassName('item-grid')[0].innerHTML+=generateItemCard(itemLines[i]);
+        }//Dirt-1/48=An essential block for filling up holes and adding that natural look that's so... natural.^ffffff
+    }
+}
+
+function generateItemCard(line) {
+    let pric = parseInt(line.split('-')[1].split("=")[0].split("/")[0],10);
+    console.log('Line is '+line);
+    console.log('pric is '+pric);
+    
+    let am=parseInt(line.split('-')[1].split("=")[0].split("/")[1],10);
+    console.log("color is "+line.split("^")[1]);
+    let item = new Item(line.split('-')[0],pric,am,Math.round(pric/am * 10000) / 10000,line.split('-')[1].split("=")[1].split("^")[0],line.split('^')[1]);
+
+    return item.genItemHTML();
+
+}
+/*
+<div class="item-card" onclick="loadDetail('Dirt')">
+        <img src="Dirt.png" alt="Item Image">
+        <p>Dirt<br>1C for 48 (0.02C each)</p>
+        <div class="button-row">
+          <button>+</button>
+          <span style="color:white;">0</span>
+          <button>-</button>
+        </div>
+      </div>
+      <div class="item-card" onclick="loadDetail('Dirt')">
+        <img src="https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/d4/Stone.png" alt="Item Image">
+        <p>Stone<br>1C for 48 (0.02C each)</p>
+        <button id="longButton">Add 48 to cart</button>
+      </div>
+*/
 function loadDetail(name) {
   const main = document.getElementById("main");
   main.className = "detail-view";
@@ -45,17 +375,68 @@ function loadGrid() {
   `;
 }
 class ItemPair {
-  constructor(item = null, amount = 0) {
-    this.item = item;
-    this.amount = amount;
+    item="";
+    amount=0;
+  constructor(ite, amoun) {
+    item = ite;
+    amount = amoun;
   }
 }
 
 class Item {
-    name="";
-    price="";
-    numPrice=1;
-    amount=1;
-    perPrice=1.1;
+    nam="Default Name";//the name of the item
+    price=1; //the actual craften value (X in XC for YZ)
+    amount=1; //how many you get for the craften value
+    perPrice=1.1; //average price per 1
+    desc="If you're seeing this, the item probably doesn't have anything written about it yet.";//item description (not on main page)
+    color="ff0000"; //background color of the item's card
+
+    constructor(na, pric, amoun, perPric,dex,col) {
+        this.nam=na;
+        this.price=pric;
+        this.amount=amoun;
+        this.perPrice=perPric;
+        this.desc=dex;
+        this.color=col;
+    }
+    genItemHTML() {
+        let finalRes=`<div class="item-card" style="background-color:#${this.color}" onclick="loadDetail('${this.nam}')">
+        <img src="${this.nam}.png" 
+     alt="Item Image" 
+     onerror="this.onerror=null; this.src='https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/d4/${this.nam}.png'">
+     <p>${this.nam}<br>${this.price}C for ${this.amount} (${this.perPrice}C each)</p>
+
+        `;
+        let am = 0;
+        for (let i=0; i<cart.length; i++) {
+            if (typeof cart[i] == ItemPair && cart[i].item===nam) {
+                am=cart[i].amount;
+                break;
+            }
+        }
+        if (am==0) {
+            finalRes+=`<button id="longButton">Add ${this.amount} to cart</button>
+      </div>`;
+        } else finalRes+=`<div class="button-row">
+          <button>+</button>
+          <span style="color:white;">${this.am}</span>
+          <button>-</button>
+        </div>`;
+        return finalRes;
+        
+        /* option 
+        <div class="item-card" onclick="loadDetail('Dirt')">
+        <img src="Dirt.png" alt="Item Image">
+        <p>Dirt<br>1C for 48 (0.02C each)</p>
+        <div class="button-row">
+          <button>+</button>
+          <span style="color:white;">0</span>
+          <button>-</button>
+        </div>
+      </div>
+        */
+    }
 
 }
+
+loadMainPage ()
