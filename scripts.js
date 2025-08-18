9bead7f40c12d35dd86a00dd3070448e8eb95859
SquireCoding <ben@holyjoemail.com>
2025-08-17 19:15:17 -0500

    Added more stuff

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
+var cart=[]
+
+
+class ItemPair {
+  constructor(item = null, amount = 0) {
+    this.item = item;
+    this.amount = amount;
+  }
+}
+
 class Item {
-    title="";
+    name="";
+    price="";
+    numPrice=1;
+    amount=1;
+    perPrice=1.1;
 
 }
